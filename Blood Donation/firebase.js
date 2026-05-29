89fd53ceed18601c33939b57dfa0b6e1da71cc28
kalpanat-hub <kalpanatkalpanat42@gmail.com>
2026-05-29 19:33:24 +0530

    Add files via upload

---
+++ Blood Donation/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDAbZqpnvj0HrhjTwP9kSV_Pn3adqOFuzI",
+  authDomain: "bloodbridge-ca8b4.firebaseapp.com",
+  projectId: "bloodbridge-ca8b4",
+  storageBucket: "bloodbridge-ca8b4.firebasestorage.app",
+  messagingSenderId: "477571925690",
+  appId: "1:477571925690:web:d12236443b24af63c12483",
+};
